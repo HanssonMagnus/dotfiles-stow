c610e6dabbfae7148cc2c968207abe2601ca455c
Magnus Hansson <hansson.carl.magnus@gmail.com>
2023-09-15 12:30:54 +0200

    Add conf Kitty and Nvim

---
+++ nvim/.config/nvim/README.md
@@ -0,0 +1,12 @@
+# Install packer
+```
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+```
+
+# Install plugins
+```
+:PackerSync
+```
+
+# Tree
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,58 @@
 -- Maintainer: Magnus Hansson
--- Last change: 2022-08-11
+-- Last change: 2022-08-15
 
+--------------------------------------------------------------------------------
+-- Load packer plugins, run :PackerSync to install
+--------------------------------------------------------------------------------
+-- Load lua/packes-plugins.lua
+require('user/packer-plugins')
+
+-- Lualine
+require('lualine').setup({
+    options = {
+        theme = 'dracula',
+    }
+})
+require('lualine').setup()
+
+-- Bufferline
+require("bufferline").setup{}
+
+-- Treesitter
+require('user/treesitter')
+
+--------------------------------------------------------------------------------
+-- Import settings
+--------------------------------------------------------------------------------
+require('user/options')
+require('user/keymaps')
+require('user/indent-blankline')
+
+--------------------------------------------------------------------------------
+-- Python PEP 8
+--------------------------------------------------------------------------------
+--vim.o.textwidth = 79
+--vim.o.colorcolumn = 80
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+--------------------------------------------------------------------------------
+-- LaTeX
+--------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------
+-- Misc.
+--------------------------------------------------------------------------------
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]]
+})
+
+-- Reload whenever .Xresources is updated
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "~/.Xresources" },
+  command = [[!xrdb %]]
+})
